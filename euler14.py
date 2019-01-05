29b9d146c8db4bc9c2551a60ac24b5865794eb51
baloghd <balogh.domonkos@gmail.com>
2019-01-05 19:05:56 +0100

    14

--- euler14.py
+++ euler14.py
@@ -2,8 +2,6 @@ from eulerutil.number_theory import *
 
 known = dict()
 for i in range(2, 1_000_000):
-    if i in known.keys():
-        continue
     seq = []
     n = i
     marlatotthossz = 0
@@ -14,9 +12,7 @@ for i in range(2, 1_000_000):
         else:
             n = 3*n + 1
         if n in known.keys():
-            # print(f"láttuk már n-t ({int(n)})")
             marlatotthossz = known[n]
             break
-    
     known[i] = len(seq) + marlatotthossz
-    # print(i, known[i], seq, known)
+print(max(known, key = known.get))
