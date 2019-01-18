1c3aad81fd99cd72335f5763a842185af1f0a469
baloghd <balogh.domonkos@gmail.com>
2019-01-18 18:04:35 +0100

    36

--- euler36.py
+++ euler36.py
@@ -1,12 +1,14 @@
 def palindromic(n: int or str) -> bool:
     s = str(n)
-    for index, char in enumerate(s):
-        if index + 1 <= len(s)//2:
-            if s[index] != s[-(index + 1)]:
-                return False
-    return True
+    #for index, char in enumerate(s):
+        #if index + 1 > len(s)//2:
+            #break
+        #if s[index] != s[-(index + 1)]:
+            #return False
+    #return True
+    return s == s[::-1]
 
 acc = 0
-for i in range(1_000_000):
+for i in range(1, 1_000_000, 2):
     if palindromic(i) and palindromic(bin(i)[2:]):
        acc += i
